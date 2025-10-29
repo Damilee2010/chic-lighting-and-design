a07416c4aa4cdb690b4b3df81817b195ff120aba
Dammiee <rachaelfun10@gmail.com>
2025-10-29 11:51:13 +0100

    updated media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// App.js
 import React from "react";
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Hero from "./Components/Hero";
 import ProductGrid from "./Components/ProductGrid";
